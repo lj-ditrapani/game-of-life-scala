2084c170a88f9663508d0d1dbcb59311515f5490
Lyall Jonathan Di Trapani <lj.ditrapani@gmail.com>
2016-04-16 13:39:12 -0400

    Split LifeSpec into grid-spec & cell-spec
    Use shish-kabob file naming.

---
+++ src/test/scala/cell-spec.scala
@@ -0,0 +1,103 @@
+package info.ditrapani.game_of_life
+
+import org.scalatest.{FunSpec, Matchers}
+
+class CellSpec extends FunSpec with Matchers {
+  describe("Cell Class") {
+    describe("alive") {
+      it("returns false if dead") {
+        Cell.dead.alive should be (false)
+      }
+
+      it("returns true if alive") {
+        Cell.living.alive should be (true)
+      }
+    }
+
+    describe("to_char") {
+      it("returns + if alive") {
+        Cell.living.to_char should be ('+')
+      }
+
+      it("it returns - if dead") {
+        Cell.dead.to_char should be ('-')
+      }
+    }
+
+    describe("next") {
+      def run_tests(base_cell: Cell, tests: List[(Int, Cell)]): Unit = {
+        for (test <- tests) {
+          val (count, expected_cell) = test
+          val state = if (expected_cell.alive) "living" else "dead"
+          it(s"returns the $state Cell if neighbor_count == $count") {
+            base_cell.next(count) should be (expected_cell)
+          }
+        }
+      }
+
+      describe("when cell is alive,") {
+        val tests = List(
+          (0 -> Cell.dead),
+          (1 -> Cell.dead),
+          (2 -> Cell.living),
+          (3 -> Cell.living),
+          (4 -> Cell.dead),
+          (5 -> Cell.dead)
+        )
+
+        run_tests(Cell.living, tests)
+      }
+
+      describe("when cell is dead,") {
+        val tests = List(
+          (0 -> Cell.dead),
+          (1 -> Cell.dead),
+          (2 -> Cell.dead),
+          (3 -> Cell.living),
+          (4 -> Cell.dead),
+          (5 -> Cell.dead)
+        )
+
+        run_tests(Cell.dead, tests)
+      }
+    }
+  }
+
+  describe("Cell Object") {
+    describe("get(Boolean)") {
+      it("returns Cell.living if true") {
+        Cell.get(true) should be (Cell.living)
+      }
+
+      it("returns Cell.dead if false") {
+        Cell.get(false) should be (Cell.dead)
+      }
+    }
+
+    describe("get(Char)") {
+      it("returns the dead cell if char is -") {
+        Cell.get('-') should be (Cell.dead)
+      }
+
+      it("returns the alive cell if char is +") {
+        Cell.get('+') should be (Cell.living)
+      }
+    }
+
+    describe("living") {
+      it("returns singleton living cell") {
+        val cell = Cell.living
+        cell should be (Cell.living)
+        cell should === (Cell(true))
+      }
+    }
+
+    describe("dead") {
+      it("returns singleton dead cell") {
+        val cell = Cell.dead
+        cell should be (Cell.dead)
+        cell should === (Cell(false))
+      }
+    }
+  }
+}
---
+++ src/test/scala/life-spec.scala
@@ -0,0 +1,6 @@
+package info.ditrapani.game_of_life
+
+import org.scalatest.{FunSpec, Matchers}
+
+class LifeSpec extends FunSpec with Matchers {
+}
